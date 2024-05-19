5a8e66194af574c0576e6a528b0939e30fc2ea59
GeraltDima <166142979+GeraltDima@users.noreply.github.com>
2024-05-19 19:12:01 +0200

    Save_3

--- KeyBoard.py
+++ KeyBoard.py
@@ -1,6 +1,53 @@
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-button_1 = KeyboardButton("Направления")
-button_2 = KeyboardButton("О IT-Cube")
-menu_kb1 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_1, button_2)
+#============================================================================
+menu_kb1 = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text="Направления"), KeyboardButton(text="О IT-Cube🎲")]
+],
+        resize_keyboard=True,
+        input_field_placeholder="Выберите пункт в меню."
+)
 
+menu_kb2 = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text="Программирование на Python"), KeyboardButton(text="VR/AR-разработка")],
+        [KeyboardButton(text="Веб-разработка"), KeyboardButton(text="Мобильная разработка")],
+        [KeyboardButton(text="Системное администрирование"), KeyboardButton(text="Беспилотные авиационные системы")],
+        [KeyboardButton(text="⬅️Назад"), KeyboardButton(text="3D-моделирование")]
+],
+        resize_keyboard=True,
+        input_field_placeholder="Выберите пункт в меню."
+)
+inline_kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Перейти на сайт IT-Cube", url="https://it-cube39.ru/directions")]
+        ])
+#============================================================================
+
+#============================================================================
+inline_kb1 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Доп. информация", callback_data="info1")]
+])
+
+inline_kb2 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Доп. информация", callback_data="info2")]
+])
+
+inline_kb3 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Доп. информация", callback_data="info3")]
+])
+
+inline_kb4 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Доп. информация", callback_data="info4")]
+])
+
+inline_kb5 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Доп. информация", callback_data="info5")]
+])
+
+inline_kb6 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Доп. информация", callback_data="info6")]
+])
+
+inline_kb7 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Доп. информация", callback_data="info7")]
+])
+#============================================================================
--- Programma_proect.py
+++ Programma_proect.py
@@ -1,51 +1,113 @@
-from aiogram import Dispatcher, Bot, executor, types
-from config import TOKEN
-from KeyBoard import menu_kb1
+import asyncio
 
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, CallbackQuery
+
+from config import TOKEN
+
+import KeyBoard as kb
+from Texts import direction_1, direction_2, direction_3, direction_4, direction_5, direction_6, direction_7, about_us, full_directions
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 
-@dp.message_handler(commands=['start'])
-async def start_command(msg: types.Message):
-    await msg.reply("Привет, напиши /help чтобы узнать для чего этот бот!")
+@dp.message(CommandStart())
+async def start_command(message: Message):
+    await message.reply("Привет! Нажми сюда➡️/help⬅️ чтобы узнать для чего создан этот бот!", reply_markup=kb.menu_kb1)
 
 
-@dp.message_handler(commands=['help'])
-async def help_command(msg:types.Message):
-    await msg.reply("Данный бот отправляет разную информацию о IT-Cube", reply_markup=menu_kb1)
+@dp.message(Command("help"))
+async def help_command(message: Message):
+    await message.reply("Данный бот отправляет разную информацию о IT-Cube", reply_markup=kb.inline_kb)
 
-@dp.message_handler(commands=['Программирование на Python'])
-async def info_1(msg:types.Message):
-    await msg.answer("""В ходе курса вы научитесь:"
-                     "- строить алгоритмы программ;"
-                     "– писать чистый и грамотный код;"
-                     "– работать в современных IDE;"
-                     "- писать программы для решения конкретных задач""")
-
-@dp.message_handler(commands=['VR/AR-разработка'])
-async def info_1(msg:types.Message):
-    await msg.answer("""В ходе курса вы научитесь:"
-                     "- научитесь работать с игровыми движками (Unity и Unreal Engine);"
-                     "– научитесь создавать ассеты для своих приложений (уровней или сцен);"
-                     "– сможете создавать сложные сцены и уровни для виртуальной реальности""")
-
-@dp.message_handler(commands=['VR/AR-разработка'])
-async def info_1(msg:types.Message):
-    await msg.answer("""""")
-
-@dp.message_handler(text=['Направления'])
-async def direction(msg:types.Message):
-    await msg.answer("""Направления IT-Cube:"
-                     "•Программирование на Python"
-                     "•VR/AR-разработка"
-                     "•Веб-разработка"
-                     "•Мобильная разработка"
-                     "•Системное администрирование"
-                     "•Беспилотные авиационные системы"
-                     "•Лицей Академии Яндекса""")
+@dp.message(F.text == 'Направления')
+async def direction(message: Message):
+    await message.answer(full_directions, reply_markup=kb.menu_kb2)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+#============================================================================
+
+@dp.message(F.text == 'О IT-Cube🎲')
+async def info_0(message: Message):
+    await message.answer(about_us)
+
+#=================||===============================||======================
+#=================||=====Н-А-П-Р-А-В-Л-Е-Н-И-Я=====||======================
+#=================\/===============================\/======================
+
+@dp.message(F.text == 'Программирование на Python')
+async def info_1(message: Message):
+    await message.answer(direction_1, reply_markup=kb.inline_kb1)
+
+@dp.callback_query(F.data == 'info1')
+async def clb_info1(callback: CallbackQuery):
+    await callback.answer("Преподователь: Велиев Рафаель Ибрагимович\n2 группы; в одной группе - 12 человек", show_alert=True)
+
+#============================================================================
+@dp.message(F.text == 'VR/AR-разработка')
+async def info_2(message: Message):
+    await message.answer(direction_2, reply_markup=kb.inline_kb2)
+
+@dp.callback_query(F.data == 'info2')
+async def clb_info2(callback: CallbackQuery):
+        await callback.answer("Преподователь: ФИО\n2 группы; в одной группе - 12 человек", show_alert=True)
+
+# ============================================================================
+
+@dp.message(F.text == 'Веб-разработка')
+async def info_3(message: Message):
+    await message.answer(direction_3, reply_markup=kb.inline_kb3)
+
+@dp.callback_query(F.data == 'info3')
+async def clb_info3(callback: CallbackQuery):
+        await callback.answer("Преподователь: Елизаров Даниил Александрович\n2 группы; в одной группе - 12 человек", show_alert=True)
+
+#============================================================================
+
+@dp.message(F.text == 'Мобильная разработка')
+async def info_4(message: Message):
+    await message.answer(direction_4, reply_markup=kb.inline_kb4)
+
+@dp.callback_query(F.data == 'info4')
+async def clb_info4(callback: CallbackQuery):
+        await callback.answer("Преподователь: ФИО\n2 группы; в одной группе - 12 человек", show_alert=True)
+
+#============================================================================
+
+@dp.message(F.text == 'Системное администрирование')
+async def info_5(message: Message):
+    await message.answer(direction_5, reply_markup=kb.inline_kb5)
+
+@dp.callback_query(F.data == 'info5')
+async def clb_info5(callback: CallbackQuery):
+        await callback.answer("Преподователь: Бобровский Александр Владимирович\n2 группы; в одной группе - 12 человек", show_alert=True)
+
+#============================================================================
+
+@dp.message(F.text == 'Беспилотные авиационные системы')
+async def info_6(message: Message):
+    await message.answer(direction_6, reply_markup=kb.inline_kb6)
+
+@dp.callback_query(F.data == 'info6')
+async def clb_info6(callback: CallbackQuery):
+        await callback.answer("Преподователь: Буистов Владислав Валерьевич\n2 группы; в одной группе - 12 человек", show_alert=True)
+
+#============================================================================
+
+@dp.message(F.text == '3D-моделирование')
+async def info_7(message: Message):
+    await message.answer(direction_7, reply_markup=kb.inline_kb7)
+
+@dp.callback_query(F.data == 'info7')
+async def clb_info7(callback: CallbackQuery):
+        await callback.answer("Преподователь: ФИО\n2 группы; в одной группе - 12 человек", show_alert=True)
+
+#============================================================================
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
